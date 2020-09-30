a5cba11e77e09563d3a345a3dd0843acb601344f
Jeswinek <jeswinjaison7@gmail.com>
2020-09-30 21:51:03 +0530

    color change

--- lib/register.dart
+++ lib/register.dart
@@ -12,7 +12,7 @@ class _signUpState extends State<signUp> {
       home: Scaffold(body:
       Container(child:
       Stack(children:[
-        Container( color: Colors.red,
+        Container( color: Colors.green,
         ),
         Padding(
           padding: EdgeInsets.only(top: 90, left: 10, right: 10),
