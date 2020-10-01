1c4ebadea0542a881f18fb8c873d9ec9d1760922
Jeswinek <jeswinjaison7@gmail.com>
2020-10-02 01:59:47 +0530

    Colors corrected

---
+++ lib/Switches.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Switch extends StatefulWidget {
+  @override
+  _SwitchState createState() => _SwitchState();
+}
+
+class _SwitchState extends State<Switch> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('jdkjdjad'))
+      ),
+    );
+  }
+}
--- lib/automation.dart
+++ lib/automation.dart
@@ -9,166 +9,186 @@ class automation extends StatefulWidget {
 }
 
 class _automationState extends State<automation> {
-
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back),
-            onPressed: () =>Navigator.pop(context),
+          appBar: AppBar(backgroundColor: Colors.green[800],
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+            title: Text('home'),
           ),
-          title: Text('home'),
-        ),
-        body:Column(
-          children: [
-            _Security(),_plug(),
-            _overuse(),
-          ],
-        )
-      ),
-
+          body: Stack(children:[
+            Container(
+    color: Colors.green[200],),
+              Column(children:[
+              _Security(),
+              _plug(),
+              _overuse(),
+      ],),
+        ],),
+    ),
     );
   }
+
   Widget _Security() {
     return Container(
-    margin: EdgeInsets.all(10),
-      child:  Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-    child: InkWell(
-    onTap: () {
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>
-    signUp()),);
-    },
-    child: Column(
-    children: <Widget>[
-    ClipRRect(
-    borderRadius: BorderRadius.only(
-    topLeft: Radius.circular(8.0),
-    topRight: Radius.circular(8.0),
-    ),
-    child: Image.asset('image/intruder.jpg',
-    width:MediaQuery.of(context).size.width,
-    height: 150,
-    fit:BoxFit.fill
-
-    ),
-    ),
-    ],
-    ),
-    ),
-    ),
-    );
-  }
-Widget _plug() {
-  return Container(
-    child: Column(
-      children: [
-        Padding(
-          child: Container(
-            height: 110,
-            color: Colors.green,
-            child: Row(
-              children: [
-                Padding(
-                  child:
-                  Text('Set Your Plug Time',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontWeight: FontWeight.bold,
-                      fontSize: 30, color: Colors.white,),
-                  ),
-                  padding: EdgeInsets.only(left: 40, right: 20),
+      color: Colors.lightGreenAccent[400],
+      height: 180,
+      margin: EdgeInsets.all(10),
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => signUp()),
+            );
+          },
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
                 ),
-                Padding(
-                  child:
-                  Icon(
-                    Icons.timer, size: 45,
-                  ),
-                  padding: EdgeInsets.only(right: 20),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Image.asset('image/intruder.jpg',
+                      width: MediaQuery.of(context).size.width,
+                      height: 150,
+                      fit: BoxFit.fill),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-        ),
-        Padding(
-          child:
-          Container(
-            width: MediaQuery
-                .of(context)
-                .size
-                .width,
-            height: 30,
-            decoration: BoxDecoration(
-              border: Border(
-                left: BorderSide(width: 2.0, color: Colors.green),
-                bottom: BorderSide(width: 2.0, color: Colors.green),
-                right: BorderSide(width: 2.0, color: Colors.green),
-
-              ),
-              color: Colors.white,
-            ),
-            child: Padding(
-              child:
-              Text('Timer is off',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  wordSpacing: 2,
-                  letterSpacing: 1.5,
-                ),
-              ),
-              padding: EdgeInsets.only(top: 4, left: 40),
-            ),
-          ),
-          padding: EdgeInsets.only(left: 10, right: 10),
-        ),
-      ],
-    ),
-  );
-}
-Widget _overuse() {
-  return Container(
-    height:150,
-    margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-    child:  Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-      color: Colors.lightGreen,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>
-              signUp()),);
-        },
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(8.0),
-                topRight: Radius.circular(8.0),
-              ),
-              child:Padding(
-                child:
-              Center(
-                child: Text('switch',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,fontStyle: FontStyle.normal,
-                      color: Colors.red,fontSize: 40,
-                      letterSpacing: 3,fontFamily: "Broadway",
-                    )),
-              ),
-                padding: EdgeInsets.only(top:50),
-              ),
-            ),
-          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 
-}
+  Widget _plug() {
+    return Container(
+      color: Colors.transparent,
+      height: 150,
+      margin: EdgeInsets.fromLTRB(10,5, 10,5),
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        color: Colors.greenAccent[400],
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => signUp()),
+            );
+          },
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+                child: Padding(
+                  child:Row(children:[ Text(
+                    'Set Your Plug Timer',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: "Broadway",
+                    ),
+                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Icon(Icons.alarm_add, size: 50,),
+                    )
+                ],
+                ),
+                  padding: EdgeInsets.only(top: 45, left: 10, bottom: 3),
+              ),
+          ),
+              Padding(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      //left: BorderSide(width: 2.0, color: Colors.green),
+                      top: BorderSide(width: 2.0, color: Color(0x31cc43)),
+                      //right: BorderSide(width: 2.0, color: Colors.green),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    child: Text(
+                      'Timer is off',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        wordSpacing: 2,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    padding: EdgeInsets.only(top: 4, left: 40),
+                  ),
+                ),
+                padding: EdgeInsets.only(top: 10),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-
+  Widget _overuse() {
+    return Container(
+      height: 140,
+      margin: EdgeInsets.fromLTRB(10,10, 10,5),
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0))),
+        color: Colors.greenAccent[400],
+        child: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => signUp()),
+            );
+          },
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(8.0),
+                  topRight: Radius.circular(8.0),
+                ),
+                child: Padding(
+                  child: Center(
+                    child: Text('Switch',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          color: Colors.lightGreen[100],
+                          fontSize: 40,
+                          letterSpacing: 3,
+                          fontFamily: "Broadway",
+                        )),
+                  ),
+                  padding: EdgeInsets.only(top: 40),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
