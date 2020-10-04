702ecec65a2972d30492fc882bfd81a6b1bd6437
Jeswinek <jeswinjaison7@gmail.com>
2020-10-04 19:17:29 +0530

    firebase auth added

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'Switches.dart';
 
 class loginUser extends StatefulWidget {
