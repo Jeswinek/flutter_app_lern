31bd49baa258d96906d121cd929e221d9fd44d66
Jeswinek <jeswinjaison7@gmail.com>
2020-10-04 19:17:29 +0530

    firebase auth added

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class signUp extends StatefulWidget {
   @override
