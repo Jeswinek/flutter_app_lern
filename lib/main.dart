4f838cd460c06a336ee1a3958c71731cc18c6258
Rejulr <rejulraphael@gmail.com>
2021-01-09 14:08:30 +0530

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
               ),
               child:
               Stack(children:[ CurvedShape(),
-                Register()
+                energy()
                 // Register(),
               ]
               ),
