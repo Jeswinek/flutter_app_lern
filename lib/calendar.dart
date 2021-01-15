74f85da0597e9a4d334690cf301cea63eb0cfab0
Rejulr <rejulraphael@gmail.com>
2021-01-15 17:44:09 +0530

    commited

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,19 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_app_lern/calendar.dart';
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: Calendar(),
-    );
-  }
-}
 
 class Calendar extends StatefulWidget {
   @override
