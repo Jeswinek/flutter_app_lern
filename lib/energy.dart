ecf770d320fd0c23460c2782850e1f5a27f3ea21
Jeswinek <jeswinjaison7@gmail.com>
2021-01-16 14:46:56 +0530

    ok

--- lib/energy.dart
+++ lib/energy.dart
@@ -1,14 +1,18 @@
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
-import'package:flutter/material.dart';
+import'values.dart';
 import 'package:flutter/painting.dart';
 import'package:flutter/widgets.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:charts_flutter/flutter.dart'as charts;
 import 'package:url_launcher/url_launcher.dart';
 import 'graph.dart';
-
+import 'package:firebase_database/firebase_database.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 class menu extends StatefulWidget {
   @override
   _menuState createState() => _menuState();
@@ -16,6 +20,7 @@ class menu extends StatefulWidget {
 
 class _menuState extends State<menu> {
   @override
+
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
@@ -23,38 +28,33 @@ class _menuState extends State<menu> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-             DrawerHeader(
-          child: Container(
-            height: 64,
-            child: Row(
-              children: <Widget>[
-              CircleAvatar(
-              radius: 32,
-              backgroundImage:AssetImage('image/Logoo.png'
+            DrawerHeader(
+              child: Container(
+                height: 64,
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 32,
+                      backgroundImage:AssetImage('image/Logoo.png'
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('KEERTHY M S'),
+                        Text('Choondal'),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(width: 16),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('KEERTHY M S'),
-                Text('Choondal'),
-              ],
-            ),
-            ],
-          ),
-        ),
-               // child: Text(
-               //   'Advanced Menu',
-               //   style: TextStyle(color: Colors.white, fontSize: 25),
-               // ),
               decoration: BoxDecoration(
 
                 color: Colors.blueGrey,
               ),
             ),
-
             ListTile(
               leading: Icon(Icons.person_outline),
               title: Text('User'),
@@ -94,6 +94,93 @@ class energy extends StatefulWidget {
 }
 
 class _energyState extends State<energy> {
+  final fb = FirebaseDatabase.instance.reference();
+  var retrievedName = "";
+  String name = "";
+  String Times = "";
+  String k = "";
+  String Datte = "";
+  int Time; //its poornesh created time
+  String y = "";
+  String p = "";
+  String seconds = "";
+  var ref;
+  String costs = "";
+  double unit=180;
+  String textHolder = 'Old Sample Text...!!!';
+  int bal;
+  int rem;
+  double readingUnit = 0;
+  double priceElectricity = 0;
+
+  changeText(String j) {
+    setState(() {
+      textHolder = j;
+    });
+  }
+
+  priceCalculation() {
+    if (unit >=0 && unit <=50) {
+      priceElectricity = unit * 2.90;
+    }
+    else if (unit >=51 && unit<=100) {
+      priceElectricity = unit * 3.70;
+    } else if (unit >= 101 && unit<=150) {
+      priceElectricity = unit * 4.80;
+    }else if (unit >= 151 && unit<=200) {
+      priceElectricity = unit * 6.40;
+    }else if(unit>=201) {
+      priceElectricity = unit * 7.50;
+    }
+    else print("unit doesnot calculated");
+    setState(() {
+      costs ="$priceElectricity";
+    });
+
+  }
+
+  samplefunction(){
+    final ref=fb.reference().child("");
+    DateTime now=DateTime.now();
+    print("${now.second}:${now.microsecond}");
+    seconds="${now.second}";
+    Time = int.parse(seconds);
+    rem = Time%5;
+    bal=Time-rem;
+    Datte="${now.year}${now.month}${now.day}$bal";
+    print(Datte);
+    ref.child(Datte).once().then((DataSnapshot data){
+      setState(() {
+        retrievedName=data.value ;
+        // cost(retrievedName);
+      });
+    });
+  }
+  samplefunction2() {
+    final ref = fb.reference().child("");
+    DateTime now=DateTime.now();
+    print("${now.second}:${now.microsecond}");
+    seconds="${now.second}";
+    Time = int.parse(seconds);
+    rem = Time%5;
+    bal=Time-rem;
+    print(now.month);
+    Datte="yyyy:$bal";
+    //  ref=fb.reference().child("")
+    ref.child(Datte).set(name);
+    y = name;
+  }
+  samplefunction5() {
+    DateTime now=DateTime.now();
+    print("${now.second}:${now.microsecond}");
+    seconds="${now.second}";
+    Time = int.parse(seconds);
+    rem = Time%5;
+    setState(() {
+      bal=Time-rem;
+      //  Time=Time-rem;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -141,139 +228,180 @@ class _energyState extends State<energy> {
                 ),
                 height:120,
                 width: 270,
-                child:
-                Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 10, left: 5.0, right: 0.0,bottom: 10),
-                      child: GestureDetector(
-                        onTap: () => {
-                        },
-                        child: ClipOval(
-                          child:Padding(child: Container(
 
-                            height:     65.0,// height of the button
-                            width: 55.0, // width of the button
-                            child: Center(child: Text('price')),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
-                              shape: BoxShape.circle,
-                              color:  Colors.white70,
+                child: Text('$textHolder ',
+                    style: TextStyle(fontSize: 21)),
+
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 5.0, right: 0.0,bottom: 10),
+                    child: GestureDetector(
+                      onTap: () => {
+                        {
+                          // setState(() {
+                          // samplefunction();
+                          // k=retrievedName;
+                          // changeText();
+                          // })
+                          samplefunction(),
+                          changeText(retrievedName),
+                          // samplefunction5()
+                        }
+                      },
+                      child: ClipOval(
+                        child:Padding(child: Container(
+
+                          height:     65.0,// height of the button
+                          width: 55.0, // width of the button
+                          child: Center(child: Text('Units')),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
                             ),
+                            shape: BoxShape.circle,
+                            color:  Colors.white70,
                           ),
-                            padding: EdgeInsets.only(left: 8,right: 8),
+                        ),
+                          padding: EdgeInsets.only(left: 8,right: 8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
+                    child: GestureDetector(
+                      onTap: () =>
+                      {
+                        samplefunction(),
+                        priceCalculation(),
+                        changeText(costs),
+                      }
+                      ,
+                      child: ClipOval(
+                        // height of the button
+                        child: Container(
+                          height: 65.0,
+                          width: 55.0, // width of the button
+                          child: Center(child: Text('cost')),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
+                            ),
+                            shape: BoxShape.circle,
+                            color:  Colors.white70,
                           ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
-                      child: GestureDetector(
-                        onTap: () =>
-                        {
-                        },
-                        child: ClipOval(
-                          child: Container(
-                            height: 65.0, // height of the button
-                            width: 55.0, // width of the button
-                            child: Center(child: Text('numb')),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
-                              shape: BoxShape.circle,
-                              color:  Colors.white70,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
+                    child: GestureDetector(
+                      onTap: () =>
+                      {
+                      },
+                      child: ClipOval(
+                        child: Container(
+
+                          height: 65.0, // hei
+                          // ght of the button
+                          width: 55.0, // width of the button
+                          child: Center(child: Text('cost')),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
                             ),
+
+
+
+                            shape: BoxShape.circle,
+                            color:  Colors.white70,
                           ),
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
-                      child: GestureDetector(
-                        onTap: () =>
-                        {
-                        },
-                        child: ClipOval(
-                          child: Container(
+                  ),
 
-                            height: 65.0, // hei
-                            // ght of the button
-                            width: 55.0, // width of the button
-                            child: Center(child: Text('cost')),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
 
-                              shape: BoxShape.circle,
-                              color:  Colors.white70,
+                  Padding(
+                    padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
+                    child: GestureDetector(
+                      onTap: () =>
+                      {
+                      },
+                      child: ClipOval(
+                        child: Container(
+                          height: 65.0, // hei
+                          // ght of the button
+                          width: 55.0, // width of the button
+                          child: Center(child: Text('data')),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 2,
                             ),
+
+                            shape: BoxShape.circle,
+                            color:  Colors.white70,
                           ),
                         ),
                       ),
                     ),
-
-
-                    Padding(
-                      padding: EdgeInsets.only(top:10,bottom: 10, left: 5, right:0.0),
-                      child: GestureDetector(
-                        onTap: () =>
-                        {
-                        },
-                        child: ClipOval(
-                          child: Container(
-
-                            height: 65.0, // hei
-                            // ght of the button
-                            width: 55.0, // width of the button
-                            child: Center(child: Text('data')),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.black,
-                                width: 2,
-                              ),
-
-                              shape: BoxShape.circle,
-                              color:  Colors.white70,
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.all(1),
+                padding: EdgeInsets.all(8),
+                height: 83,
+                width: 438,
+                alignment: Alignment.bottomRight,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Card(
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.black, width: 2.0),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: FlatButton.icon(
+                                  icon: Icon(Icons.show_chart,size: 48,),
+                                  label: Text(
+                                    "Graphical Representation",
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => HomePage()),
+                                    );
+                                  },
+                                )
                             ),
-                          ),
-                        ),
+                          ]
                       ),
                     ),
                   ],
-                ),
-              ),
 
-              Container(
-               // child: ,
-                margin: EdgeInsets.all(30),
-                padding: EdgeInsets.all(1000),
-                height: 207,
-                width: 450,
-                decoration: new BoxDecoration(
-                  color: Colors.white60,
-                  border: Border.all(
-                      color: Colors.black, // set border color
-                      width: 3.0), // set border width
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(15.0)), // set rounded corner radius
-                  boxShadow: [
-                    BoxShadow(blurRadius: 20,
-                        color: Colors.white30,
-                        offset: Offset(1, 8))],
                 ),
               ),
               Container(
                 margin: EdgeInsets.all(0),
                 padding: EdgeInsets.all(0),
                 height: 83,
-                width: 180,
+                width: 250,
                 alignment: Alignment.center,
 
                 child: Column(
@@ -286,7 +414,7 @@ class _energyState extends State<energy> {
                         borderRadius: BorderRadius.circular(0.0),
                       ),
                       child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: <Widget>[
                             Padding(
                                 padding: const EdgeInsets.all(3.0),
@@ -294,7 +422,7 @@ class _energyState extends State<energy> {
                                   icon: Icon(Icons.calendar_today),
                                   label: Text(
                                     "calendar",
-                                    style: TextStyle(fontSize: 15),
+                                    style: TextStyle(fontSize: 25),
                                   ),
                                   onPressed: () => launch(""),
                                 )
@@ -345,6 +473,14 @@ class _energyState extends State<energy> {
             ],
           ),
         ),
+      ),
+    );
+  }
+  Widget cost(String val){
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 30, 10, 5),
+        child: Text(val),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_lern/baseHome.dart';
+import 'package:provider/provider.dart';
+import 'values.dart';
+//import 'package:flutter_app_lern/value.dart';
+import 'home_screen.dart';
+import 'loginpage.dart';
+import 'signuppage.dart';
+import 'models/authentication.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_app_lern/energy.dart';
-import 'package:flutter_app_lern/graph.dart';
-import 'Home.dart';
+import 'package:flutter_app_lern/loginpage.dart';
+import 'package:flutter_app_lern/signuppage.dart';
+//import 'Home.dart';
+//import 'package:flutter_app_lern/view.dart';
 import 'package:flutter/services.dart';
 import 'curve.dart';
+import 'baseHome.dart';
+import 'graph.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'Home.dart';
 
-void main()=> runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'I will learn',
-      theme: ThemeData(
-        //canvasColor: Colors.transparent,
-        primaryColor: Color(0xFF80E1D1),
-        hintColor: Color(0xFFC0F0E8),),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Authentication(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Energy meter,home automation,security',
+        theme: ThemeData(
+          primaryColor: Colors.green,
+        ),
         home: Scaffold(
-        body: Center(
-          child: Container(
-        constraints: BoxConstraints.expand(),
-    decoration: BoxDecoration(
-    // image: DecorationImage(
-    // image: AssetImage('image/Logoo.jpg'),
-    // fit: BoxFit.cover)
-    ),
-      child:Homescreen(),
-    //   Stack(children:[ CurvedShape(),
-    //     HomePage(),
-    // ]
-    // ),
-    ),
-    ),
-    ),
-    );
+          body: Center(
+            child: Container(
+              constraints: BoxConstraints.expand(),
+              decoration: BoxDecoration(
+                // image: DecorationImage(
+                // image: AssetImage('image/Logoo.jpg'),
+                // fit: BoxFit.cover)
+              ),
+              child:
+              Stack(children:[ CurvedShape(),
+                energy()
+                //Register(),
+              ]
+              ),
+            ),
+          ),
+        ),
+        //LoginScreen(),
+        routes: {
+          SignupScreen.routeName: (ctx)=> SignupScreen(),
+          LoginScreen.routeName: (ctx)=> LoginScreen(),
+          HomeScreen.routeName: (ctx)=> HomeScreen(),
+          Homescreen.routeName: (ctx)=> Homescreen(),
 
+        },
+      ),
+    );
   }
 }
