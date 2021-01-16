e781713f7837717954b7c20d511dd6f6908f2212
Jeswinek <jeswinjaison7@gmail.com>
2021-01-16 15:25:36 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,9 @@ import 'graph.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'Home.dart';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+//import 'calendar.dart';
 
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
@@ -49,8 +49,9 @@ class MyApp extends StatelessWidget {
               ),
               child:
               Stack(children:[ CurvedShape(),
-                energy()
+                energy(),
                 //Register(),
+                //Calendar()
               ]
               ),
             ),
@@ -61,7 +62,7 @@ class MyApp extends StatelessWidget {
           SignupScreen.routeName: (ctx)=> SignupScreen(),
           LoginScreen.routeName: (ctx)=> LoginScreen(),
           HomeScreen.routeName: (ctx)=> HomeScreen(),
-          Homescreen.routeName: (ctx)=> Homescreen(),
+
 
         },
       ),
